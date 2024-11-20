a56d589c77f56da8c8773f69cefaf44e8fa6105c
Max Svidlo <svid320@gmail.com>
2024-11-20 21:06:27 +0200

    why am I such an idiot

--- discord.go
+++ discord.go
@@ -58,10 +58,12 @@ func sendDiscordMessage(messageBytes []byte) {
 	}
 	msg := formatMessage(data)
 
-	if data.Payload.PriceChangeRatio > 0 && data.Payload.Volume > 500000 && data.Payload.Price > 5 {
-		sendMessage(BullChannelId, msg)
-	} else {
-		sendMessage(BearChannelId, msg)
+	if data.Payload.Volume > 500000 && data.Payload.Price > 5 {
+		if data.Payload.PriceChangeRatio > 0 {
+			sendMessage(BullChannelId, msg)
+		} else {
+			sendMessage(BearChannelId, msg)
+		}
 	}
 }
 
