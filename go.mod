1dbd347e60007bf9898909d14fcd513cacb36325
Max Svidlo <svid320@gmail.com>
2024-11-20 20:44:30 +0200

    lots of stuff

--- go.mod
+++ go.mod
@@ -3,14 +3,15 @@ module github.com/svidlak/momentum-scanner-websocket
 go 1.22.0
 
 require (
-	github.com/bwmarrin/discordgo v0.28.1 // indirect
+	github.com/bwmarrin/discordgo v0.28.1
+	github.com/gorilla/websocket v1.5.3
+	github.com/joho/godotenv v1.5.1
+	github.com/shirou/gopsutil v3.21.11+incompatible
+)
+
+require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
-	github.com/gorilla/websocket v1.5.3 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
-	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
-	github.com/shirou/gopsutil/v3 v3.24.5 // indirect
-	github.com/shoenig/go-m1cpu v0.1.6 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
---
+++ server_status.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
+)
+
+type ServerStatus struct {
+	CPUUsage         string          `json:"cpu_usage"`
+	MemUsed          string          `json:"mem_used"`
+	MemTotal         string          `json:"mem_total"`
+	Alive            bool            `json:"alive"`
+	LastMessage      json.RawMessage `json:"last_message"`
+	ConnectedClients int             `json:"connected_clients"`
+}
+
+func serverStatusHandler(w http.ResponseWriter, r *http.Request) {
+	cpuPercent, _ := cpu.Percent(0, false)
+	memStats, _ := mem.VirtualMemory()
+
+	cpuUsage := fmt.Sprintf("%.2f%%", cpuPercent[0])
+	memUsed := formatMemory(memStats.Used)
+	memTotal := formatMemory(memStats.Total)
+
+	status := ServerStatus{
+		CPUUsage:         cpuUsage,
+		MemUsed:          memUsed,
+		MemTotal:         memTotal,
+		Alive:            true,
+		LastMessage:      lastWebSocketMessage,
+		ConnectedClients: len(clients),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(status)
+}
+
+func formatMemory(bytes uint64) string {
+	if bytes >= 1<<30 {
+		return fmt.Sprintf("%.2f GB", float64(bytes)/(1<<30))
+	} else if bytes >= 1<<20 {
+		return fmt.Sprintf("%.2f MB", float64(bytes)/(1<<20))
+	}
+	return fmt.Sprintf("%d KB", bytes/1024)
+}
